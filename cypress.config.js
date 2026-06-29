8c9c197ff24b8b2cff240d43b6f77d94bbda3fbf
24101274-oss <24101274@ue.edu.pe>
2026-06-29 00:51:32 -0500

    cambios git

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,8 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000/',
+    experimentalMemoryManagement: true,
+    numTestsKeptInMemory: 0,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
