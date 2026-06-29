f3e80ada2384d3125e64c59893d97162ad97d31d
24101274-oss <24101274@ue.edu.pe>
2026-06-29 01:20:54 -0500

    Revertir config de memoria para evitar crash en Electron

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,6 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000/',
-    experimentalMemoryManagement: true,
-    numTestsKeptInMemory: 0,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
