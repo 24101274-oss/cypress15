e81a8be3817b4052237aecd6c8e40f6320e22b5e
24101274-oss <24101274@ue.edu.pe>
2026-06-29 01:36:23 -0500

    Revertir config de memoria para evitar crash en Electron

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,17 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000/',
+    numTestsKeptInMemory: 0,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.name === 'electron') {
+          // Apaga la aceleración gráfica para evitar el crash en GitHub Actions
+          launchOptions.args.push('--disable-gpu');
+          // Optimiza el uso de la memoria RAM del servidor
+          launchOptions.args.push('--disable-dev-shm-usage');
+        }
+        return launchOptions;
+      });
     },
   },
 });
